89cd998ffb4d8d6250ecef498504bb05b528d5bf
katsuyukishimbo <katsuyuki0611@gmail.com>
2020-02-04 23:11:38 +0900

    third person scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+  [SerializeField]
+  int maxScore = 999999;
+  int score = 0;
+  public int Score
+  {
+    set
+    {
+      score = Mathf.Clamp(value, 0, maxScore);
+    }
+    get
+    {
+      return score;
+    }
+  }
+  public void Awake()
+  {
+    if (this != Instance)
+    {
+      Destroy(gameObject);
+      return;
+    }
+    DontDestroyOnLoad(gameObject);
+  }
+  void Start()
+  {
+    
+  }
+  void Update()
+  {
+        
+  }
+
